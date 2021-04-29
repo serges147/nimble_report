24fc182e599d88490de5177c2eb855e609d5f7fe
Sergei Shirokov <sshirokov@malwarebytes.com>
2021-04-29 10:38:10 +0300

    tests to reproduce

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -2,13 +2,32 @@
 //  Tests.swift
 //  Tests
 //
-//  Created by Sergei Shirokov on 4/29/21 .
-//
 
+import Nimble
 import XCTest
 
 class Tests: XCTestCase {
 
-    func test() throws {
+    struct Xxx: Equatable {
+
+        let value: Int
+    }
+
+    func testCompiles() throws {
+
+        let xxx = Xxx(value: 42)
+        expect(xxx) == Xxx(value: 42)
+    }
+
+    func testDoesNotCompile__but_used_to_on_xcode_12_4() throws {
+
+        let xxx = Xxx(value: 13)
+        expect(xxx) == .init(value: 13)
+    }
+
+    func testWorkaround() throws {
+
+        let xxx = Xxx(value: 7)
+        expect(xxx) == .some(.init(value: 7))
     }
 }
